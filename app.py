3ff8dab7f0fe40deb4fa8d20e6f52382c1236530
Karen Vanessa <vane.200014@gmail.com>
2023-03-14 12:34:03 -0500

    hecho

--- app.py
+++ app.py
@@ -1,40 +1,25 @@
-from flask import Flask, request, jsonify, send_file
-from psycopg2 import connect, extras
+from os import name                 #nombra el metodo flask
+from threading import main_thread   #trae el modulo main
+from flask import Flask, send_file             #incluye todo el framework
+from flask import render_template, request, flash, redirect, url_for, session, logging           #permite mostrar todos los templates
+from psycopg2 import psycopg2, connect, extras                  #permite la conexion a la bdd
 
-app = Flask(__name__)
+app = Flask(__name__)               #crear la aplicacion
+app.secret_key = 'secret'
 
-<<<<<<< HEAD
-host= 'localhost'
-port='5432'
-bdname='bd_iec'
-user='postgres'
-password='2580'
-
-def get_connection():
-    conn = connect(host=host, port=port, dbname=dbname, user=user, password=password)
-=======
-host = 'localhost'
+host = "localhost"
 port = 5432
-dbname = 'bd_iec'
-user = 'postgres'
-password = '200494'
+dbname = "bd_iec"
+user = "postgres"
+password = "2580"
 
-
-def get_connection():
-    conn = connect(host=host, port=port, dbname=dbname,user=user, password=password)
->>>>>>> Esteban
+def get_connection():                                                                          #llamando al modulo de la bdd
+    conn = psycopg2.connect("host=localhost port=5432 dbname=bd_iec user=postgres password=2580")       #inicializa la conexion a la bdd
     return conn
 
 @app.get('/')
 def home():
-<<<<<<< HEAD
-    return send_file("login/login.py")
+    return send_file('templates/alumnos/vistaA.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
-=======
-    return send_file('static/vistaA.html')
-    
 if __name__ == '__main__':
     app.run(debug=True)
->>>>>>> Esteban
