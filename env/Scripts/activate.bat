d9cfd6e729ce68fed42996d37c1981a832cf4716
Karen Vanessa <vane.200014@gmail.com>
2023-03-27 15:35:12 -0500

    final

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,11 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
+<<<<<<< HEAD
 set VIRTUAL_ENV=C:\xampp\htdocs\Login_py\env
+=======
+set VIRTUAL_ENV=C:\Users\Vanessa\OneDrive\Documentos\flask_app\env
+>>>>>>> d2355293b409560a818bf4aa09f1761b2fd2377e
 
 if not defined PROMPT set PROMPT=$P$G
 
